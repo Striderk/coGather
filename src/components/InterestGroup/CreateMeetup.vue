be4af3e930e41ded437b60afe47f8e36bcda54c6
Savanna Du <savanna.du@hotmail.com>
2018-10-31 23:29:09 +0800

    updated date picker

--- src/components/InterestGroup/CreateMeetup.vue
+++ src/components/InterestGroup/CreateMeetup.vue
@@ -18,7 +18,7 @@
                 required></v-text-field>
             </v-flex>
         </v-layout>
-        <v-loayout row>
+        <v-layout row>
           <v-flex xs12 sm6 offset-sm3>
             <v-text-field
               name="location"
@@ -27,8 +27,8 @@
               v-model="location"
               required></v-text-field>
           </v-flex>
-        </v-loayout>
-        <v-loayout row>
+        </v-layout>
+        <v-layout row>
           <v-flex xs12 sm6 offset-sm3>
             <v-text-field
               name="imageUrl"
@@ -37,13 +37,13 @@
               v-model="imageUrl"
               required></v-text-field>
           </v-flex>
-        </v-loayout>
-        <v-loayout row>
+        </v-layout>
+        <v-layout row>
           <v-flex xs12 sm6 offset-sm3>
             <img :src="imageUrl" height="150">
           </v-flex>
-        </v-loayout>
-        <v-loayout row>
+        </v-layout>
+        <v-layout row>
           <v-flex xs12 sm6 offset-sm3>
             <v-text-field
               name="description"
@@ -53,7 +53,24 @@
               v-model="description"
               required></v-text-field>
           </v-flex>
-        </v-loayout>
+        </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <h4>Choose a Date & Time</h4>
+            </v-flex>
+          </v-layout>
+          <v-layout>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-date-picker v-model="date"></v-date-picker>
+              <p>{{ date }}</p>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-time-picker v-model="time" format="24hr" ></v-time-picker>
+              <p>{{ time }}</p>
+            </v-flex>
+          </v-layout>
         <v-layout row>
           <v-flex xs12 sm6 offset-sm3>
             <v-btn
@@ -69,26 +86,43 @@
 </template>
 
 <script>
-  export default{
+  export default {
     data () {
       return {
         title: '',
         location: '',
         imageUrl: '',
-        description: ''
+        description: '',
+        date: new Date().toISOString().substr(0, 10),
+        time: null,
+        picker: new Date().toISOString().substr(0, 10)
       }
     },
     computed: {
-      formIsValid (){
-        return this.title !== ''&&
-          this.location !=='' &&
-          this.imageUrl !=='' &&
+      formIsValid () {
+        return this.title !== '' &&
+          this.location !== '' &&
+          this.imageUrl !== '' &&
           this.description !== ''
+      },
+      submittableDateTime () {
+        const date = new Date(this.date)
+        if (typeof this.time === 'string') {
+          let hours = this.time.match(/^(\d+)/)[1]
+          const minutes = this.time.match(/:(\d+)/)[1]
+          date.setHours(hours)
+          date.setMinutes(minutes)
+        } else {
+          date.setHours(this.time.getHours())
+          date.setMinutes(this.time.getMinutes())
+        }
+
+        return date
       }
     },
     methods: {
       onCreateMeetup () {
-        if (!this.formIsValid){
+        if (!this.formIsValid) {
           return
         }
         const meetupData = {
@@ -96,7 +130,7 @@
           location: this.location,
           imageUrl: this.imageUrl,
           description: this.description,
-          date: new Date()
+          date: this.submittableDateTime
         }
         this.$store.dispatch('createMeetup', meetupData)
         this.$router.push('/meetups')
